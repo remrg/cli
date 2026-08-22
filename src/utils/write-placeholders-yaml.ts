14e80c60ab203f8aade3381c66ec3730dbd47714
Drew Immerman <30954849+DrewImm@users.noreply.github.com>
2026-08-22 15:21:11 -0400

    fix: remrg placeholder goes in wrong folder (#10) (#11)
    Co-authored-by: Drew Immerman <drew@stateless.studio>

--- src/utils/write-placeholders-yaml.ts
+++ src/utils/write-placeholders-yaml.ts
@@ -2,11 +2,14 @@ import { mkdirSync, writeFileSync } from 'fs';
 import path from 'path';
 import { getPlaceholdersYaml, PlaceholdersYaml } from '../remrg';
 
-export function writePlaceholdersYaml(dir: string, options: PlaceholdersYaml): void {
-	mkdirSync(path.join(dir, './remrg'), { recursive: true });
+export function writePlaceholdersYaml(
+	dir: string,
+	options: PlaceholdersYaml
+): void {
+	mkdirSync(path.join(dir, './.remrg'), { recursive: true });
 
 	writeFileSync(
-		path.join(dir, './remrg/placeholders.yaml'),
+		path.join(dir, './.remrg/placeholders.yaml'),
 		getPlaceholdersYaml(options)
 	);
 }
