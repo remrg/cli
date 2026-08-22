1969909aac8ebc6cb84152c8951c2fd484eab15c
Drew Immerman <drew@stateless.studio>
2026-08-22 15:09:42 -0400

    lint: add newline at end of placeholders.yaml

--- src/remrg/placeholders-yaml.ts
+++ src/remrg/placeholders-yaml.ts
@@ -7,5 +7,6 @@ export interface PlaceholdersYaml {
 export function getPlaceholdersYaml(options: PlaceholdersYaml): string {
 	return `name: ${options.name}
 organization: ${options.organization}
-license: ${options.license}`;
+license: ${options.license}
+`;
 }
